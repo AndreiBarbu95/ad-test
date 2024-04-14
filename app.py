59192df52040cea7c47bf8dbca638129a537f67e
Andrei Barbu <117741767+AndreiBarbu95@users.noreply.github.com>
2024-04-14 11:41:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,8 @@ hostName = sp.getoutput("hostname")
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
 
 @app.route("/")
-def hello():
-    user_agent = request.headers.get('User-Agent')
-    url = request.url
-    logging.info(f"{request.remote_addr} - - [{time.strftime('%d/%b/%Y %H:%M:%S')}] \"{request.method} {url} {request.environ.get('SERVER_PROTOCOL')}\" {user_agent}")
-    return f"<h1><center>My App v1</center></h1>"
+def root():
+  return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
